5d101f3fb62fc0a1a5394b00a0355272b6c81dc8
tyage <namatyage@gmail.com>
2015-07-28 13:45:56 +0900

    課題js-1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
+"use strict";
+
+var parseLTSVLog = (logStr) => logStr.split('\n')
+  .filter((line) => line !== '')
+  .map((line) => {
+    var obj = {};
+    line.split('\t').forEach((lv) => {
+      var [label, value] = lv.split(':');
+      obj[label] = value;
+    });
+    obj.epoch = parseInt(obj.epoch);
+    return obj;
+  });
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください

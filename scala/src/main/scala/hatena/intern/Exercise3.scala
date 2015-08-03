f0caa27e50ee8c79776ce20e8d4be5e5578488cc
tyage <namatyage@gmail.com>
2015-08-03 20:12:19 +0900

    scala課題3です

--- scala/src/main/scala/hatena/intern/Exercise3.scala
+++ scala/src/main/scala/hatena/intern/Exercise3.scala
@@ -1,8 +1,17 @@
 package hatena.intern
 
 case class LogCounter(logs: Iterable[Log]) {
-  def countError: Int = ???
+  def countError: Int = {
+    logs.filter((log) => {
+      log.status.toString.matches("""5\d\d""")
+    }).size
+  }
 
   type User = String
-  def groupByUser: Map[User, Iterable[Log]] = ???
+  def groupByUser: Map[User, Iterable[Log]] = {
+    logs.groupBy(_.user match {
+      case Some(user) => user
+      case _ => "guest"
+    })
+  }
 }
--- scala/src/test/scala/hatena/intern/Exercise3.scala
+++ scala/src/test/scala/hatena/intern/Exercise3.scala
@@ -5,7 +5,7 @@ import hatena.intern.helper._
 class Exercise3Spec extends UnitSpec {
   describe("LTSV Counter") {
 
-    val logs = LtsvParser.parse("/path/to/sample_data/log.ltsv") // リポジトリ内の`sample_data/log.ltsv`へのパスを指定してください
+    val logs = LtsvParser.parse("../sample_data/log.ltsv") // リポジトリ内の`sample_data/log.ltsv`へのパスを指定してください
 
     it("エラー数が正しくカウントされていること") {
       LogCounter(logs).countError shouldBe 2
@@ -13,13 +13,21 @@ class Exercise3Spec extends UnitSpec {
 
     it("ユーザごとにログがグループ化されていること") {
       val groupdLogs = LogCounter(logs).groupByUser
-      val franksLogs = groupdLogs.get("frank")
+      val franksLogs = groupdLogs.get("frank").getOrElse(List())
+      println(franksLogs)
 
-      groupdLogs.get("john").size shouldBe 1
-      groupdLogs.get("guest").size shouldBe 1
+      groupdLogs.get("john").getOrElse(List()).size shouldBe 1
+      groupdLogs.get("guest").getOrElse(List()).size shouldBe 1
 
       franksLogs.size shouldBe 3
       // ただしくグルーピングされているかどうかを検査するテストの続きを書いてみてください
+
+      franksLogs.count((log) => log.path == "/apache_pb.gif") shouldBe 2
+
+      groupdLogs.filter((group) => {
+        val (user, logs) = group
+        logs.find((log) => log.status == 200).isDefined
+      }).size shouldBe 2
     }
   }
 
